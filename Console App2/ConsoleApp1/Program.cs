9bfde67f9356db3a12d4381e29739efae16ce47d
Gabrovez <gabrovez321@gmail.com>
2025-09-19 16:59:09 +0300

    Шаг8 эхо

--- Console App2/ConsoleApp1/Program.cs
+++ Console App2/ConsoleApp1/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        string? input;
+        Console.WriteLine("Программа запущена. Введите текст (для выхода введите 'выход'):");
+
+        while (true)
+        {
+            Console.Write("> ");
+            input = Console.ReadLine();
+            if (input == null)
+            {
+                continue;
+            }
+            if (input.ToLower() == "выход")
+            {
+                Console.WriteLine("До встречи!");
+                break;
+            }
+            Console.WriteLine($"Вы сказали: {input}");
+        }
+    }
+}
+ 
